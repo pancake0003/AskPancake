36931bfd79b1321ed855734c552cee425042887b
pancake0003 <songyuemmm@gmail.com>
2023-12-12 16:13:38 -0500

    updated images folder and instructions txt

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const chatMessagesDiv = document.getElementById('chat-messages');
 const userMessageInput = document.getElementById('user-message');
+const randomImage = document.getElementById('random-image');
 
 function sendMessage() {
     const userMessage = userMessageInput.value;
@@ -14,7 +15,20 @@ function sendMessage() {
     // Clear the user input
     userMessageInput.value = '';
 }
+// Function to display a random image
+function displayRandomImage() {
+    const imagePaths = [
+        './images/1.png',
+        './images/2.png',
+        './images/3.png',
+        './images/3.png',
+        './images/4.png',
+    ];
 
+    const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+    //test: https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec
+    randomImage.src = 'https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec';
+}
 function sendUserMessage(message) {
     fetch('http://localhost:3000/sendMessage', {
         method: 'POST',
@@ -27,6 +41,9 @@ function sendUserMessage(message) {
         .then(data => {
             // Add AI response to the chat window
             appendMessage('ai', data.aiResponse);
+            
+            // Display a random image
+            displayRandomImage();
         })
         .catch(error => {
             console.error('Error sending message to the server:', error);
@@ -58,7 +75,10 @@ function handleKeyPress(event) {
     if (event.key === 'Enter') {
         sendMessage();
     }
+    else {
+        userMessageInput.focus();
+    }
 }
 
 // Attach the handleKeyPress function to the input field
-userMessageInput.addEventListener('keypress', handleKeyPress);
+addEventListener('keypress', handleKeyPress);
